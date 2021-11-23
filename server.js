6c398e26d60d1792eeef4e7c4e7714442f8b39c5
Nathan Vontz <nvontz@ksu.edu>
2021-11-23 13:54:08 -0600

    adding port

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.put('/image', (req, res) => {image.handleImage(req,res, db) })
 app.post('/imageurl', (req, res) => {image.handleApiCall(req,res,) })
 
 app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+  console.log(`app is running on port ${process.env.PORT}`);
 })
